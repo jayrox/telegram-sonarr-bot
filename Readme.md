d88ace3acf94cd325264eadabb248909db5a75ef
Devin Buhl <devin.kray@gmail.com>
2015-12-10 18:12:12 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,128 @@
+# telegram-sonarr-bot
+
+Bot which lets you or others add series to [Sonarr](https://sonarr.tv/).
+
+Contact [@BotFather](http://telegram.me/BotFather) on Telegram to create and get a bot token.
+
+For now, please make your bot username something unique. For example @fred-flintstone-sonarr-bot or something
+
+## Setup
+
+Make sure you have node, npm, and git installed then in shell / command prompt
+
+**Clone the repo**
+```bash
+$ git clone https://github.com/onedr0p/telegram-sonarr-bot
+```
+
+**Install the dependencies**
+```bash
+$ cd telegram-sonarr-bot
+$ npm install
+```
+
+Then move `config.json.template` to `config.json` and fill in the values.
+
+Please refer to the Sonarr specific configuration below:
+
+- **hostname**: hostname where Sonarr runs (required)
+- **apiKey**: Your API to access Sonarr (required)
+- **port**: port number Sonarr is listening on (optional, default: 5050)
+- **urlBase**: URL Base of Sonarr (optional, default: empty)
+- **ssl**: Set to true if you are connecting via SSL (default: false)
+- **username**: HTTP Auth username (default: empty)
+- **password**: HTTP Auth password (default: empty)
+
+
+**Start the bot with node, forever, pm2 or w/e**
+```bash
+$ node sonarr.js
+```
+
+## Usage
+
+Send the bot a message with the series name
+
+`/q game of`
+
+The bot will reply with
+
+```
+Found 6 series:
+1) Game of Crowns - 2014
+2) Game of Thrones - 2011
+3) Game of Silence
+4) Game of Silence (TR) - 2012
+5) The Genius Game - 2013
+6) More Than A Game - The Story of Football
+
+/s [n] to continue...
+```
+
+Send the number of the series you want with the /s flag
+
+```
+/s 2
+```
+
+The bot will ask you for the quality
+
+```
+Found 2 profiles:
+1) SD 2) HD
+
+/p [n] to continue...
+```
+
+Send the number of the profile
+
+```
+/p 2
+```
+
+Lastly, the bot will ask you where the path you want the series to go
+
+```
+Found 2 folders:
+1) /Television/Airing/
+2) /Television/Archived/
+
+/f [n] to continue...
+```
+
+Send the number of the folder
+
+```
+/f 1
+```
+
+If everything goes well, you'll see a text from the bot saying the series was added.
+
+## Changelog
+
+### v0.1.0
+- Initial release
+
+## License
+(The MIT License)
+
+Copyright (c) 2015 Devin Buhl <devin.kray@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ sonarr.js
@@ -0,0 +1,307 @@
+'use strict';
+
+var SonarrAPI = require('./lib/sonarr-api');
+var TelegramBot = require('node-telegram-bot-api');
+var NodeCache = require("node-cache");
+var _ = require("lodash");
+
+var config = require('./config.json');
+
+var bot = new TelegramBot(config.telegram.botToken, {
+  polling: true
+});
+
+var sonarr = new SonarrAPI({
+  hostname: config.sonarr.hostname,
+  apiKey: config.sonarr.apiKey,
+  port: config.sonarr.port,
+  urlBase: config.sonarr.urlBase,
+  ssl: config.sonarr.ssl,
+  username: config.sonarr.username,
+  password: config.sonarr.password
+});
+
+var cache = new NodeCache();
+
+/*
+ * get the bot name
+ */
+bot.getMe().then(function(msg) {
+  console.log('Welcome to the sonarr bot %s!', msg.username);
+}).catch(function(err) {
+  throw new Error(err);
+});
+
+/*
+ * handle start command
+ */
+bot.onText(/\/start/, function(msg) {
+  var chatId = msg.chat.id;
+  var username = msg.from.username || msg.from.first_name;
+
+  var response = [];
+
+  response.push('Hello ' + username + ', use /s to search');
+  response.push('\n`/q [series name]` to continue...');
+
+  var opts = {
+    "parse_mode": "Markdown",
+    "selective": 2,
+  };
+
+  bot.sendMessage(chatId, response.join('\n'), opts);
+});
+
+/*
+ * handle query command
+ */
+bot.onText(/\/[Qq](uery)? (.+)/, function(msg, match) {
+  var messageId = msg.message_id;
+  var chatId = msg.chat.id;
+  var fromId = msg.from.id;
+  var seriesName = match[2];
+
+  sonarr.get("series/lookup", { "term": seriesName })
+  .then(function(result) {
+    if (!result.length) {
+      throw new Error('could not find ' + seriesName + ', try searching again');
+    }
+
+    return result;
+  })
+  .then(function(series) {
+    console.log(fromId + ' requested to search for movie ' + seriesName);
+	
+    var seriesList = [];
+    var response = ['*Found ' + series.length + ' series:*'];
+
+    _.forEach(series, function(n, key) {
+
+      var id = key + 1;
+      var title = n.title;
+      var year = ("year" in n ? n.year : '');
+
+      seriesList.push({
+        "id": id,
+        "title": n.title,
+        "year": n.year,
+		"tvdbId": n.tvdbId,
+		"titleSlug": n.titleSlug,
+		"seasons": n.seasons
+      });
+
+      response.push(
+        '*' + id + '*) ' + n.title + (n.year ? ' - _' + n.year + '_' : '')
+      );
+    });
+
+    response.push('\n`/s [n]` to continue...');
+
+    // set cache
+    cache.set("seriesList" + fromId, seriesList);
+
+	// console.log(seriesList);
+	
+    return response.join('\n');
+  })
+  .then(function(response) {
+    var opts = {
+      "disable_web_page_preview": true,
+      "parse_mode": "Markdown",
+      "selective": 2,
+    };
+    bot.sendMessage(chatId, response, opts);
+  })
+  .catch(function(err) {
+    bot.sendMessage(chatId, "Oh no! " + err);
+  });
+});
+
+/*
+ * handle series selection
+ */
+bot.onText(/\/[sS](eries)? ([\d]{1})/, function(msg, match) {
+  var messageId = msg.message_id;
+  var chatId = msg.chat.id;
+  var fromId = msg.from.id;
+  var seriesId = match[2];
+  
+  // set movie option to cache
+  cache.set("seriesId" + fromId, seriesId);
+  
+  sonarr.get("profile")
+  .then(function (result) {
+    if (!result.length) {
+      throw new Error("could not get profiles, try searching again");
+    }
+	
+	if (cache.get("seriesList" + fromId) === undefined) {
+      throw new Error("could not get previous series list, try searching again");
+    }
+
+    return result;
+  })
+  .then(function(profiles) {
+    console.log(fromId + ' requested to get profiles list');
+
+    var profileList = [];
+    var response = ['*Found ' + profiles.length + ' profiles:*'];
+    _.forEach(profiles, function(n, key) {
+	  profileList.push({
+	    "id": key + 1,
+	    "name": n.name,
+	    "profileId": n.id
+	  });
+
+	  response.push('*' + (key + 1) + '*) ' + n.name);
+    });
+
+    response.push('\n`/p [n]` to continue...');
+
+    // set cache
+    cache.set("seriesProfileList" + fromId, profileList);
+
+    return response.join(' ');
+  })
+  .then(function(response) {
+    bot.sendMessage(chatId, response, {
+	  "selective": 2,
+	  "parse_mode": "Markdown"
+    });
+  })
+  .catch(function(err) {
+    bot.sendMessage(chatId, "Oh no! " + err);
+  });
+});
+
+/*
+ * handle folder selection
+ */
+bot.onText(/\/[pP](rofile)? ([\d]{1})/, function(msg, match) {
+  var messageId = msg.message_id;
+  var chatId = msg.chat.id;
+  var fromId = msg.from.id;
+  var profileId = match[2];
+  
+  // set movie option to cache
+  cache.set("seriesProfileId" + fromId, profileId);
+  
+  sonarr.get("rootfolder")
+  .then(function (result) {
+    if (!result.length) {
+      throw new Error("could not get folders, try searching again");
+    }
+	
+	if (cache.get("seriesList" + fromId) === undefined) {
+      throw new Error("could not get previous series list, try searching again");
+    }
+
+    return result;
+  })
+  .then(function(folders) {
+    console.log(fromId + ' requested to get folder list');
+
+    var folderList = [];
+    var response = ['*Found ' + folders.length + ' folders:*'];
+    _.forEach(folders, function(n, key) {
+	  folderList.push({
+	    "id": key + 1,
+	    "path": n.path,
+	    "folderId": n.id
+	  });
+
+	  response.push('*' + (key + 1) + '*) ' + n.path);
+    });
+
+    response.push('\n`/f [n]` to continue...');
+
+    // set cache
+    cache.set("seriesFolderList" + fromId, folderList);
+
+    return response.join('\n');
+  })
+  .then(function(response) {
+    bot.sendMessage(chatId, response, {
+	  "selective": 2,
+	  "parse_mode": "Markdown"
+    });
+  })
+  .catch(function(err) {
+    bot.sendMessage(chatId, "Oh no! " + err);
+  });
+});
+
+/*
+ * handle series type selection
+ */
+bot.onText(/\/[fF](older)? ([\d]{1})/, function(msg, match) {
+  var messageId = msg.message_id;
+  var chatId = msg.chat.id;
+  var fromId = msg.from.id;
+  
+  var folderId = match[2];
+  var folderList = cache.get("seriesFolderList" + fromId);
+  
+  var profileId = cache.get("seriesProfileId" + fromId);
+  var profileList = cache.get("seriesProfileList" + fromId);
+  
+  var seriesId = cache.get("seriesId" + fromId);
+  var seriesList = cache.get("seriesList" + fromId);
+
+  if (folderList === undefined || profileList === undefined || seriesList === undefined) {
+    bot.sendMessage(chatId, 'Oh no! Error: something went wrong, try searching again');
+  }
+  
+  var series = _.filter(seriesList, function(item) {
+    return item.id == seriesId;
+  })[0];
+
+  var profile = _.filter(profileList, function(item) {
+    return item.id == profileId;
+  })[0];
+
+  var folder = _.filter(folderList, function(item) {
+    return item.id == folderId;
+  })[0];
+
+  sonarr.post("series", {
+    "tvdbId": series.tvdbId,
+	"title": series.title,
+	"titleSlug": series.titleSlug,
+	"seasons": series.seasons,
+	"rootFolderPath": folder.path,
+	"seasonFolder": true,
+	"monitored": false,
+	"seriesType": "standard",
+	"qualityProfileId": profile.profileId
+	
+  })
+  .then(function(result) {
+
+    console.log(fromId + ' added series ' + series.title);
+
+    if (!result) {
+      throw new Error("could not add series, try searching again.");
+    }
+
+    bot.sendMessage(chatId, 'Series `' + series.title + '` added', {
+      "selective": 2,
+      "parse_mode": "Markdown"
+    });
+  })
+  .catch(function(err) {
+    bot.sendMessage(chatId, "Oh no! " + err);
+  })
+  .finally(function() {
+
+    // delete cache items
+    cache.del("seriesFolderList" + fromId);
+	
+    cache.del("seriesProfileId" + fromId);
+    cache.del("seriesProfileList" + fromId);
+	
+    cache.del("seriesId" + fromId);
+    cache.del("seriesList" + fromId);	
+  });
+  
+});
